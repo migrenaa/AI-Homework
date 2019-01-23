f32c1bcab74f6bd1b5212eca139451bcc2353e64
migrenaa <mimonova13@gmail.com>
2019-01-23 08:02:52 +0200

    initialized the solution

---
+++ AI-Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AI_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
